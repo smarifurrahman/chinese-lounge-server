06381a5348cbd45424a666a3f8268b0b527cb58b
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-03 05:58:07 +0600

    chef data res send and image link fixed

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+const chefs = require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('chinese lounge server is running..');
 });
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
 
 app.listen(port, () => {
     console.log(`chinese lounge api is running on port: ${port}`);
